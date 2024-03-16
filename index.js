66be6d28b161f0d5cccfc171ae707ca445b7addc
guillermo-mercado <150946772+guillermo-mercado@users.noreply.github.com>
2024-03-16 21:32:30 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,86 @@
+import express from "express";
+import bodyParser from "body-parser";
+import pg from "pg";
+
+const app = express();
+const port = 3000;
+
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "permalist",
+  password: "password",
+  port: 5432,
+});
+db.connect();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+let items = [
+  { id: 1, title: "Buy milk" },
+  { id: 2, title: "Finish homework" },
+];
+
+
+
+const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+const days = ["Sunday", "Monday", "Tuesday","Wednesday", "Thursday", "Friday", "Saturday"];
+const d = new Date();
+let month = months[d.getMonth()];
+let day = days[d.getDay()];
+
+
+
+app.get("/", async (req, res) => {
+  try {
+    const result = await db.query("SELECT * FROM items ORDER BY id ASC");
+    items = result.rows;
+    res.render("index.ejs", {
+      listTitle: "Daily tasks",
+      listItems: items,
+      month: month,
+      day:day,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+
+});
+
+app.post("/add", async(req, res) => {
+  const item = req.body["newItem"];
+  // items.push({title: item});
+  try {
+    await db.query("INSERT INTO items (title) VALUES ($1)", [item]);
+    res.redirect("/");
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+app.post("/edit", async (req, res) => {
+  const id = req.body["updatedItemId"];
+  const item = req.body["updatedItemTitle"]
+    try {
+      await db.query("UPDATE items SET title = ($1) WHERE id = $2", [item, id]);
+      res.redirect("/");
+    } catch (err) {
+      console.log(err);
+    }
+  });
+
+
+app.post("/delete", async (req, res) => {
+  const id = req.body["deleteItemId"];
+    try {
+      await db.query("DELETE FROM items WHERE id = $1", [id]);
+      res.redirect("/");
+    } catch (err) {
+      console.log(err);
+    }
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
